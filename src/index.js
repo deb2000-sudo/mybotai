9be77db9534e658d9c9d72b256a646578ef98c61
deb2000-sudo <cdnayak2000@gmail.com>
2024-09-30 14:47:40 +0530

    sidebar completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.js';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/Home.jsx';
+import History from './pages/History/History.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// Define the router with paths and components
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "history",
+        element: <History />
+      },
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+]);
 
+// Get the root element and render the app
+// const check=document.getElementById('root')
+// console.log(check);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
-
---
+++ src/pages/History/History.jsx
@@ -0,0 +1,9 @@
+const History=()=>{
+    return(
+        <div>
+            <h1>This is history page</h1>
+        </div>
+    )
+}
+
+export default History;
