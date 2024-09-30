5bdc2e10543d2100e470aeb063ca69716917b272
deb2000-sudo <cdnayak2000@gmail.com>
2024-09-30 17:58:08 +0530

    history page added

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,3 +1,9 @@
+import { Typography, Box, Stack, Divider, Select, MenuItem } from '@mui/material'
+import { useEffect, useState } from 'react'
+import ChatHistoryCard from '../../components/ChatHistoryCard/ChatHistoryCard'
+import ChatFilter from '../../components/ChatFilter/ChatFilter'
+import Navbar from '../../components/NavBar/Navbar'
+
 const History=()=>{
     return(
         <div>
